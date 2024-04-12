af870149ed7cfc9ff404dcf041f5f3a00033cbdd
StarJwlly <>
2024-04-12 16:36:54 -0300

    feat(p1-pdmt) exibe lista ficticia

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import TarefaEntrada from './TarefaEntrada'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import TarefaLista from './TarefaLista'
 
 
 export default class App extends React.Component{
   render(){
     return (
         <div>
-            <div className='row'>
-                <div className='col-1'/>
+            <div className='row justify-content-center'>
                 <div className='col-10'>
                     <TarefaEntrada></TarefaEntrada>
                 </div>
             </div>
-            
+            <div className='row justify-content-center'>
+                <div className='col-10'>
+                    <TarefaLista></TarefaLista>
+                </div>
+            </div>
         </div>
     )
   }
