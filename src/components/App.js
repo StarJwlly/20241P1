38eb37856fae10d386d5faad0a38abbe98118af4
StarJwlly <>
2024-04-12 15:32:09 -0300

    feat(p1-pdmt) cria componente principal e faz hello

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+export default class App extends React.Component{
+  render(){
+    return (
+      <div>
+        hello tarefas
+      </div>
+    )
+  }
+}
