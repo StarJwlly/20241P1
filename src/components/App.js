aead19234c97e63dc944d73fc38f284e9cb226a2
StarJwlly <>
2024-04-12 16:15:10 -0300

    refactor(p1-pdmt) remove classe de coluna do componente TarefaEntrada

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,20 @@ import React from 'react'
 import TarefaEntrada from './TarefaEntrada'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+
 export default class App extends React.Component{
   render(){
     return (
         <div>
-            <TarefaEntrada></TarefaEntrada>
-
+            <div className='row'>
+                <div className='col-1'/>
+                <div className='col-10'>
+                    <TarefaEntrada></TarefaEntrada>
+                </div>
+            </div>
+            
         </div>
     )
   }
 }
+
--- src/components/TarefaEntrada.js
+++ src/components/TarefaEntrada.js
@@ -7,14 +7,8 @@ export default class TarefaEntrada extends Component {
   render() {
     return (
       <form onSubmit={this.onFormSubmit}>
-            <div className='row'>
-                <div className='col-1'></div>
-                <input className='col-10 mt-2' type='text' id='novaTarefaInput' placeholder='Digite a descrição de uma nova tarefa'></input>
-            </div>
-            <div className='row'>
-                <div className='col-1'></div>
-                <button type="button" className="btn btn-primary col-10 mt-2" id='novaTarefaButton'>OK</button>
-            </div>
+        <input className='col-12 mt-2' type='text' id='novaTarefaInput' placeholder='Digite a descrição de uma nova tarefa'></input>
+        <button type="button" className="btn btn-primary col-12 mt-2" id='novaTarefaButton'>OK</button>
       </form>
     )
   }
